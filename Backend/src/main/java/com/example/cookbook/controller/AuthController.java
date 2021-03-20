dcb204d2e2fb51faa72b4c26c16d15f0175e6d4b
Lucio Bevilacqua <fanacdc2@gmail.com>
2021-03-20 19:00:30 -0300

    FIX: signup response to match with frontend

--- Backend/src/main/java/com/example/cookbook/controller/AuthController.java
+++ Backend/src/main/java/com/example/cookbook/controller/AuthController.java
@@ -3,9 +3,13 @@ package com.example.cookbook.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.example.cookbook.dto.SignupDTO;
 import com.example.cookbook.dto.UserDTO;
 import com.example.cookbook.service.AuthService;
+import com.example.cookbook.utils.ResponseDTO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,10 +26,13 @@ public class AuthController {
 	private AuthService authService;
 
     @RequestMapping(value = "/signup", method = RequestMethod.POST)
-    ResponseEntity<UserDTO> signup(@RequestBody SignupDTO signupDTO) {
+    ResponseEntity<ResponseDTO> signup(@RequestBody SignupDTO signupDTO) {
+		Map<String, Object> data = new HashMap<>();
         try {
             UserDTO newUserDTO = authService.signup(signupDTO);
-            return ResponseEntity.status(HttpStatus.CREATED).body(newUserDTO);
+			System.out.println("newUserDTO: " + newUserDTO.toString()); 
+			data.put("user", newUserDTO.toJson());
+            return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDTO(true, data, "", ""));
         } catch(Exception exception) {
             throw new ResponseStatusException(
            HttpStatus.INTERNAL_SERVER_ERROR, "Sign up error", exception);
